3fc23d6fa3cd411e39809233e9658f6eb1d2473d
development6 <40081976+development6@users.noreply.github.com>
2021-09-24 18:33:05 -0300

    commit2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductoComponent} from './general/producto/producto.component';
+import {RegistroComponent} from './general/registro/registro.component';
+import {LoginComponent} from './general/login/login.component';
+import {HomeComponent} from './general/home/home.component';
+import { NotFoundComponent } from './general/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent}, //ruta por default
+  {path: 'producto', component:ProductoComponent}, 
+  {path: 'registro', component:RegistroComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'notFound', component:NotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HeaderComponent } from './compartida/header/header.component';
 import { FooterComponent } from './compartida/footer/footer.component';
 import { AsideComponent } from './compartida/aside/aside.component';
 import { LoginComponent } from './general/login/login.component';
+import { RegistroComponent } from './general/registro/registro.component';
+import { ProductoComponent } from './general/producto/producto.component';
+import { HomeComponent } from './general/home/home.component';
+import { NotFoundComponent } from './general/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { LoginComponent } from './general/login/login.component';
     HeaderComponent,
     FooterComponent,
     AsideComponent,
-    LoginComponent
+    LoginComponent,
+    RegistroComponent,
+    ProductoComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
