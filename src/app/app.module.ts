ac0da8e1d0248e8de72591684e8b0e0237758673
development6 <40081976+development6@users.noreply.github.com>
2021-09-24 15:19:55 -0300

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './compartida/header/header.component';
+import { FooterComponent } from './compartida/footer/footer.component';
+import { AsideComponent } from './compartida/aside/aside.component';
+import { LoginComponent } from './general/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
